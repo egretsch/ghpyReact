95c499d833b9cc8a449a8b1ca1f6e031f6dd8b81
Edward Gretsch <edwardgretsch@gmail.com>
2018-11-28 16:15:07 -0700

    Update DisplayImages.js

--- client/src/components/DisplayImages/DisplayImages.js
+++ client/src/components/DisplayImages/DisplayImages.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 import "./DisplayImages.css";
-
+// takes the array of images and displays them on the page
 class DisplayImages extends Component {
 
     render() {
 
         return (
             <div className="giphyPic">
-
+                
                 {this.props.items.length > 0 && this.props.items.map(({ images }) => (
                     <img src={images.preview_gif.url} margin = "5px" height="200px" />
                     ))
